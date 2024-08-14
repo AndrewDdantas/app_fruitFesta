67e30285e2528aa3b3469b540ba77eee02906473
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-08-13 21:33:24 -0300

    Add files via upload

---
+++ pages/Roterizador.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import services.connect as func
+from io import BytesIO
+
+st.set_page_config(
+    page_title="Roterizador",
+    page_icon="",
+    layout="wide", 
+    initial_sidebar_state="collapsed",
+)
+
+try:
+    log = st.session_state['Login']
+except:
+    st.switch_page('./main.py')
+
+
+if 'uploaded_file' not in st.session_state:
+    st.session_state.uploaded_file = None
+
+arq_st = st.empty()
+arquivo = arq_st.file_uploader("Favor inserir arquivo.")
+
+df_amostra = st.empty()
+
+if arquivo or st.session_state.uploaded_file:
+    st.session_state.uploaded_file = arquivo or st.session_state.uploaded_file
+
+    aggDF = pd.read_excel(st.session_state.uploaded_file).groupby(['ClienteDesc','Geolocalização']).agg({
+            'OV': lambda x: ', '.join(map(str, x)), 
+            'Nota': lambda x: ', '.join(map(str, x)),
+        'Volumes': 'sum',
+        'TOTAL': 'sum'
+    }).reset_index()
+
+    aggDF['Geolocalização'] = aggDF['Geolocalização'].apply(func.ajustar_cord)
+    aggDF['Geolocalização'] = aggDF['Geolocalização'].replace(r'\s+', '', regex=True)
+    aggDF['Geolocalização'] = aggDF['Geolocalização'].replace('\n', '', regex=False)
+
+    waypoints = aggDF['Geolocalização'].values.tolist()
+    dist = pd.DataFrame(func.gerar_endereco(waypoints=waypoints))
+
+    aggDF = pd.merge(aggDF, dist, 'left', left_on='Geolocalização', right_on=1)
+    aggDF = aggDF.sort_values(2, ascending=False) 
+    df_visu = aggDF[['OV', 'Nota', 'ClienteDesc', 'Geolocalização', 0, 2, 'Volumes', 'TOTAL']]
+    df_visu.columns = ['Ordem Venda', 'Notas', 'Cliente', 'Localização', 'Endereço', 'Distância', 'Volumes', 'Total']
+    df_visu['Distância'] = df_visu['Distância'].apply(lambda x: f'{int(x/1000)} Km'.replace(',','.'))
+    df_visu['Volumes'] = df_visu['Volumes'].apply(lambda x: "{:,.0f}".format(x).replace(',', 'X').replace('.', ',').replace('X', '.'))
+    df_visu['Total'] = df_visu['Total'].apply(lambda x: "{:,.0f}".format(x).replace(',', 'X').replace('.', ',').replace('X', '.'))
+
+    df_amostra.dataframe(df_visu, hide_index=True)
+
+    button = st.button('Gerar Romaneio')
+
+    if button:
+        df_amostra.empty()
+        rotas = func.divide_em_blocos(aggDF['Geolocalização'].values.tolist(), 20)
+        aggDF = aggDF[['Nota', 'OV', 'ClienteDesc', 'Volumes', 'TOTAL', 0]]
+        merge = pd.merge(aggDF, pd.DataFrame(func.gerar_rota(rotas=rotas)), 'right', left_on=0, right_on='endereço')
+        merge = merge[['Nota', 'OV', 'endereço', 'distância', 'duração', 'ClienteDesc', 'Volumes', 'TOTAL']].fillna(0)
+        volumes = "{:,.0f}".format(merge['Volumes'].sum()).replace(',', 'X').replace('.', ',').replace('X', '.')
+        duracao = func.converter_segundos(merge['duração'].sum())
+        distancia = f'{int(merge['distância'].sum()/1000)} Km'.replace(',','.')
+        merge['distância'] = merge['distância'].apply(lambda x: f'{int(x/1000)} Km'.replace(',','.'))        
+        merge['duração'] = merge['duração'].apply(func.converter_segundos)
+        doc = func.criar_romaneio(merge, volumes, duracao, distancia)
+
+        buffer = BytesIO()
+        doc.save(buffer)
+        buffer.seek(0)
+        
+        st.download_button(
+            label="Baixar Romaneio",
+            data=buffer,
+            file_name="romaneio_gerado.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+        arq_st.empty()  
+        df_amostra.empty()
+        st.session_state.uploaded_file = None
---
+++ services/connect.py
@@ -0,0 +1,116 @@
+import googlemaps
+from datetime import datetime, timedelta
+import pandas as pd
+import streamlit as st
+
+
+minha_chave_api = st.secrets['key']
+gmaps = googlemaps.Client(key=minha_chave_api)
+
+def converter_segundos(segundos):
+    dias = segundos // 86400
+    horas = (segundos % 86400) // 3600
+    return f'{dias} dia(s) e {horas} hora(s)'
+
+def ajustar_cord(row):
+
+    comma_positions = [i for i, char in enumerate(row) if char == ',']
+
+    if len(comma_positions) == 3 and comma_positions[0] in [2,3,4]:
+        row = row.replace(',','.', 1)
+        row = row[:comma_positions[2]] + '.' + row[comma_positions[2] + 1:]
+        return row.replace(' ','')
+    else:
+        return row.replace(' ','')
+
+def gerar_endereco(gmaps=gmaps, origem='R. Uirapuru, 316 - Santa Monica, Feira de Santana - BA, 44078-250',waypoints=None):
+    matrix = gmaps.distance_matrix(origins=[origem], destinations=waypoints, mode="driving")
+    address_distance_list = []
+    for destination, way,element in zip(matrix['destination_addresses'], waypoints, matrix['rows'][0]['elements']):
+        if element['status'] == 'OK':
+            distance = element['distance']['value']  # Distância em metros
+        else:
+            distance = None  # No caso de falha na busca
+        
+        address_distance_list.append((destination,way, distance))
+    distancia = pd.DataFrame(address_distance_list).drop_duplicates()
+    return distancia
+
+def divide_em_blocos(lista, tamanho_bloco):
+    origem = '-12.2653916,-38.9302185'
+    blocos = []
+    lista.insert(0,origem)
+    for i in range(0, len(lista), tamanho_bloco):
+        bloco = lista[i:i+tamanho_bloco]
+        if i != 0:
+            bloco.insert(0, blocos[-1][-1])
+        blocos.append(bloco)
+    bloco.insert(len(blocos[-1]), origem)
+    return blocos
+
+def gerar_rota(rotas,gmaps=gmaps):
+    informacoes = []
+    agora = datetime.now()
+    for i in rotas:
+        direcoes = gmaps.directions(i[0],
+                                    i[-1],
+                                    mode="driving",
+                                    waypoints=i[1:-1],
+                                    optimize_waypoints=True,
+                                    departure_time=agora)
+        for leg in direcoes[0]['legs']:
+            endereco = leg['end_address']
+            distancia = leg['distance']['value']
+            duracao = leg['duration']['value']
+            informacoes.append({'endereço': endereco, 'distância': distancia, 'duração':duracao})
+    return informacoes
+
+
+from docx import Document
+from docx.shared import Pt
+
+def adicionar_linha_horizontal():
+    o = ''
+    i = 1
+    while i <= 183:
+        o += "_" 
+        i += 1
+    return o
+
+# Função para substituir as chaves no texto
+def substituir_chaves(paragraphs, substitutions):
+    for paragraph in paragraphs:
+        for key, value in substitutions.items():
+            if key in paragraph.text:
+                paragraph.text = paragraph.text.replace(key, str(value))
+                
+def criar_romaneio(merge,volumes, tempo, km):
+# Abrir o documento existente
+    doc = Document('./romaneio_modelo.docx')
+
+    # Dicionário com as chaves e os valores para substituição
+    substituicoes = {
+        '<<tt_rota>>': km,
+        '<<pc>>': f'{volumes} peças',
+        '<<tm>>': tempo
+    }
+
+    # Substituir as chaves no documento
+    for paragraph in doc.paragraphs:
+        substituir_chaves([paragraph], substituicoes)
+
+    # Substituir as chaves em tabelas (se houver)
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                substituir_chaves(cell.paragraphs, substituicoes)
+
+    # Lista de dados fictícios
+
+
+    # Adicionar parágrafos para cada cliente
+    for index, row in merge.iterrows():
+        # Nome do cliente
+        doc.add_paragraph(f"Cliente: {row['ClienteDesc']} | Endereço: {row['endereço']}\nAssinatura:{adicionar_linha_horizontal()}").paragraph_format.space_after = Pt(10)
+
+    return doc
