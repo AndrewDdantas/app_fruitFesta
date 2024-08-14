c40edc7ae8927dcaa023cea78e267c377c06361e
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-08-14 08:10:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 from time import sleep
 
 with st.form('LoginForm'):
+    st.title('Bem Vindo, insira seu login e senha.')
     col1, col2 = st.columns(2)
     col1.image('./logo.png')
-    col2.title('Bem Vindo, insira seu login e senha.')
     User = col2.text_input('Usuário')
 
     Pass = col2.text_input("Senha",type='password')
