b53441d244d4e4deb9f11e0c11061e858175da43
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-08-18 16:50:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ with st.form('LoginForm'):
     login_button = col2.form_submit_button("Login")
 
     if login_button:
-        if Pass == st.secrets['pass'] and User == st.secrets['user']:
+        if Pass == st.secrets['pass'] and User.lower() == st.secrets['user']:
             st.session_state['Usuário'] = User
             st.session_state['Login'] = 'Logado'
             st.warning('Login realizado com sucesso!')
             sleep(2)
-            st.switch_page('./pages/Roterizador.py')
+            st.switch_page('./pages/Home_Page.py')
             
         else:
             st.error("Favor verificar os dados.")
